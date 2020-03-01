25b91612b88db57f611aaafba39fc4e6d24567e4
bhaumikparmar <bhaumik1512@gmail.com>
2020-03-01 17:29:01 +0530

    Contact Page - 15 min aur kharab kara

---
+++ application/controllers/Contact.php
@@ -0,0 +1,15 @@
+<?php
+class Contact extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $this->load->view('Contact/index');
+    }
+
+}
+?>
