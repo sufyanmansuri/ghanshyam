9ade5f54485df965fac800d0f8042738f29a844b
sufyanmansuri <sufyan8834@gmail.com>
2020-03-01 08:23:08 +0530

    Added Login Popup

--- application/views/Home/index.php
+++ application/views/Home/index.php
@@ -4,7 +4,7 @@
 
 <head>
     <meta charset="utf-8">
-    <!--<meta http-equiv="X-UA-Compatible" content="IE=edge">-->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
     <!-- Mobile Metas -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -16,16 +16,26 @@
     <meta name="author" content="">
 
     <!-- Site Icons -->
-    <link rel="shortcut icon" href="<?= base_url('asset/image/favicon.ico') ?>" type="image/x-icon">
+    <link rel="shortcut icon" href="<?= base_url('asset/image/logo.png') ?>" type="image/x-icon">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="<?= base_url('asset/css/bootstrap.min.css') ?>">
-    <!-- Site CSS -->
-    <link rel="stylesheet" href="<?= base_url('asset/css/style.css') ?>">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+    <!-- Google Fonts Roboto -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="<?= base_url('asset/css/bootstrap.min.css')?>">
+    <!-- Material Design Bootstrap -->
+    <link rel="stylesheet" href="<?= base_url('asset/css/mdb.min.css')?>">
+    <!-- Your custom styles (optional) -->
+    <link rel="stylesheet" href="<?= base_url('asset/css/style.css')?>">
+
     <!-- Responsive CSS -->
     <link rel="stylesheet" href="<?= base_url('asset/css/responsive.css') ?>">
     <!-- Custom CSS -->
     <link rel="stylesheet" href="<?= base_url('asset/css/custom.css') ?>">
+
+    <!-- Plugin file -->
+    <link rel="stylesheet" href="<?= base_url('asset/css/addons/datatables.min.css') ?>">
 
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
@@ -38,10 +48,10 @@
     <!-- Start Main Top -->
     <header class="main-header">
         <!-- Start Navigation -->
-        <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-default bootsnav">
+        <nav class="navbar navbar-expand-lg navbar-light navbar-default bootsnav">
             <div class="container">
                 <!-- Start Header Navigation -->
-                <div class="navbar-header">
+                <div class="navbar-header" >
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-menu" aria-controls="navbars-rs-food" aria-expanded="false" aria-label="Toggle navigation">
                         <i class="fa fa-bars"></i>
                     </button>
@@ -116,7 +126,7 @@
                             <ul class="dropdown-menu">
                                 <li><a href="cart.php">Cart</a></li>
                                 <li><a href="checkout.php">Checkout</a></li>
-                                <li><a href="my-account.php">My Account</a></li>
+                                <li><a href="<?= base_url('Account') ?>">My Account</a></li>
                                 <li><a href="wishlist.php">Wishlist</a></li>
                                 <li><a href="shop-detail.php">Shop Detail</a></li>
                             </ul>
@@ -131,9 +141,13 @@
                 <div class="attr-nav">
                     <ul>
                         <li class="search"><a href="#"><i class="fa fa-search"></i></a></li>
+
+                        <!-- Login Popup Start-->
+                        <li><a href="" class="" data-toggle="modal" data-target="#modalLRForm"><i class="fas fa-user    "></i></a></li>
+                        <!-- Login Popup End-->
                         <li class="side-menu"><a href="#">
                                 <i class="fa fa-shopping-cart"></i>
-                                <span class="badge">3</span>
+                                <span class="badge badge-pill badge-danger">3</span>
                             </a></li>
                     </ul>
                 </div>
@@ -160,7 +174,7 @@
                             <p>1x - <span class="price">$40.00</span></p>
                         </li>
                         <li class="total">
-                            <a href="#" class="btn btn-default hvr-hover btn-cart">VIEW CART</a>
+                            <a href="#" class="btn hvr-hover btn-cart">VIEW CART</a>
                             <span class="float-right"><strong>Total</strong>: $180.00</span>
                         </li>
                     </ul>
@@ -187,7 +201,7 @@
     <!-- Start Slider -->
     <div id="slides-shop" class="cover-slides">
         <ul class="slides-container">
-            <li class="text-left">
+            <li class="text-center">
                 <img src="<?= base_url('asset/image/banner-01.jpg') ?>" alt="">
                 <div class="container">
                     <div class="row">
@@ -211,7 +225,7 @@
                     </div>
                 </div>
             </li>
-            <li class="text-right">
+            <li class="text-center">
                 <img src="<?= base_url('asset/image/banner-03.jpg') ?>" alt="">
                 <div class="container">
                     <div class="row">
@@ -609,17 +623,124 @@
 
     <!-- Start copyright  -->
     <div class="footer-copyright">
-        <p class="footer-company">All Rights Reserved. &copy; 2020 <a href="#">Ghanshyam Caterers</a> Design By :
-            <a href="https://html.design/">html design</a></p>
+        <p class="footer-company">All Rights Reserved. &copy; 2020 <a href="#">Ghanshyam Caterers</a></p>
     </div>
     <!-- End copyright  -->
 
     <a href="#" id="back-to-top" title="Back to top" style="display: none;">&uarr;</a>
 
-    <!-- ALL JS FILES -->
-    <script src="<?= base_url('asset/js/jquery-3.2.1.min.js') ?>"></script>
-    <script src="<?= base_url('asset/js/popper.min.js') ?>"></script>
-    <script src="<?= base_url('asset/js/bootstrap.min.js') ?>"></script>
+    <!--Modal: Login / Register Form-->
+    <div class="modal fade" id="modalLRForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog cascading-modal" role="document">
+            <!--Content-->
+            <div class="modal-content">
+
+                <!--Modal cascading tabs-->
+                <div class="modal-c-tabs">
+
+                    <!-- Nav tabs -->
+                    <ul class="nav nav-tabs md-tabs tabs-2 light-blue darken-3" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active" data-toggle="tab" href="#panel7" role="tab"><i class="fas fa-user mr-1"></i>
+                                Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-toggle="tab" href="#panel8" role="tab"><i class="fas fa-user-plus mr-1"></i>
+                                Register</a>
+                        </li>
+                    </ul>
+
+                    <!-- Tab panels -->
+                    <div class="tab-content">
+                        <!--Panel 7-->
+                        <div class="tab-pane fade in show active" id="panel7" role="tabpanel">
+
+                            <!--Body-->
+                            <div class="modal-body mb-1">
+                                <div class="md-form form-sm mb-5">
+                                    <i class="fas fa-envelope prefix"></i>
+                                    <input type="email" id="modalLRInput10" class="form-control form-control-sm validate">
+                                    <label data-error="wrong" data-success="right" for="modalLRInput10">Your email</label>
+                                </div>
+
+                                <div class="md-form form-sm mb-4">
+                                    <i class="fas fa-lock prefix"></i>
+                                    <input type="password" id="modalLRInput11" class="form-control form-control-sm validate">
+                                    <label data-error="wrong" data-success="right" for="modalLRInput11">Your password</label>
+                                </div>
+                                <div class="text-center mt-2">
+                                    <button class="btn hvr-hover" style="color: white;">Log in <i class="fas fa-sign-in ml-1"></i></button>
+                                </div>
+                            </div>
+                            <!--Footer-->
+                            <div class="modal-footer">
+                                <div class="options text-center text-md-right mt-1">
+                                    <p>Not a member? <a href="#" class="blue-text">Sign Up</a></p>
+                                    <p>Forgot <a href="#" class="blue-text">Password?</a></p>
+                                </div>
+                                <button type="button" class="btn btn-outline-info waves-effect ml-auto" data-dismiss="modal">Close</button>
+                            </div>
+
+                        </div>
+                        <!--/.Panel 7-->
+
+                        <!--Panel 8-->
+                        <div class="tab-pane fade" id="panel8" role="tabpanel">
+
+                            <!--Body-->
+                            <div class="modal-body">
+                                <div class="md-form form-sm mb-5">
+                                    <i class="fas fa-envelope prefix"></i>
+                                    <input type="email" id="modalLRInput12" class="form-control form-control-sm validate">
+                                    <label data-error="wrong" data-success="right" for="modalLRInput12">Your email</label>
+                                </div>
+
+                                <div class="md-form form-sm mb-5">
+                                    <i class="fas fa-lock prefix"></i>
+                                    <input type="password" id="modalLRInput13" class="form-control form-control-sm validate">
+                                    <label data-error="wrong" data-success="right" for="modalLRInput13">Your password</label>
+                                </div>
+
+                                <div class="md-form form-sm mb-4">
+                                    <i class="fas fa-lock prefix"></i>
+                                    <input type="password" id="modalLRInput14" class="form-control form-control-sm validate">
+                                    <label data-error="wrong" data-success="right" for="modalLRInput14">Repeat password</label>
+                                </div>
+
+                                <div class="text-center form-sm mt-2">
+                                    <button class="btn btn-info">Sign up <i class="fas fa-sign-in ml-1"></i></button>
+                                </div>
+
+                            </div>
+                            <!--Footer-->
+                            <div class="modal-footer">
+                                <div class="options text-right">
+                                    <p class="pt-1">Already have an account? <a href="#" class="blue-text">Log In</a></p>
+                                </div>
+                                <button type="button" class="btn btn-outline-info waves-effect ml-auto" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                        <!--/.Panel 8-->
+                    </div>
+
+                </div>
+            </div>
+            <!--/.Content-->
+        </div>
+    </div>
+    <!--Modal: Login / Register Form-->
+
+    <!-- jQuery -->
+    <script type="text/javascript" src="<?= base_url('asset/js/jquery.min.js') ?>"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="<?= base_url('asset/js/popper.min.js') ?>"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="<?= base_url('asset/js/bootstrap.min.js') ?>"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="<?= base_url('asset/js/mdb.min.js') ?>"></script>
+    <!-- Your custom scripts (optional) -->
+    <script type="text/javascript"></script>
+
     <!-- ALL PLUGINS -->
     <script src="<?= base_url('asset/js/jquery.superslides.min.js') ?>"></script>
     <script src="<?= base_url('asset/js/bootstrap-select.js') ?>"></script>
@@ -632,6 +753,16 @@
     <script src="<?= base_url('asset/js/form-validator.min.js') ?>"></script>
     <script src="<?= base_url('asset/js/contact-form-script.js') ?>"></script>
     <script src="<?= base_url('asset/js/custom.js') ?>"></script>
+    <!-- Plugin file -->
+    <script src="<?= base_url('asset/js/addons/datatables.min.js') ?>"></script>
+    <!-- MDBootstrap Initialize -->
+    <script>
+        $(document).ready(function() {
+            $('#dtBasicExample').DataTable();
+            $('.dataTables_length').addClass('bs-select');
+        });
+    </script>
+
 </body>
 
 </html>
