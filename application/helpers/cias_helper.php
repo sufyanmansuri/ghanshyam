1726a32325b0e75ba8b02bb013dcc62e2b17b8da
sufyanmansuri <60652760+sufyanmansuri@users.noreply.github.com>
2021-01-12 12:08:56 +0530

    Update cias_helper.php

--- application/helpers/cias_helper.php
+++ application/helpers/cias_helper.php
@@ -126,8 +126,8 @@ if (!function_exists('resetPasswordEmail')) {
             $mail->isSMTP();                                            // Send using SMTP
             $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-            $mail->Username   = 'sufyan8834@gmail.com';                     // SMTP username
-            $mail->Password   = 'Samajhmeinaayakya?';                               // SMTP password
+            $mail->Username   = '';                     // SMTP username
+            $mail->Password   = '';                               // SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
             $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
